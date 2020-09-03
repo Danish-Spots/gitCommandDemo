07c850b4bc0dde231f8ce652efcc8a12a6dcb6fe
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-09-03 12:35:58 +0200

    Initial commit of files

---
+++ gitCommandDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitCommandDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
