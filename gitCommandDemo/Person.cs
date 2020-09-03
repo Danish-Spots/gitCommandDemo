489831cc4d5fc0141c74d9c261e83b73fd18ac61
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-09-03 13:46:18 +0200

    Changed Name property to NameOfPerson in person.cs

--- gitCommandDemo/Person.cs
+++ gitCommandDemo/Person.cs
@@ -11,7 +11,7 @@ namespace gitCommandDemo
             
         }
 
-        public string Name { get; set; }
+        public string NameOfPerson { get; set; }
 
         public string Address { get; set; }
 
